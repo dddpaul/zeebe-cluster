cdfc2431761388e697c0e179e3381f293a006d09
Pavel Derendyaev <dddpaul@gmail.com>
2023-05-12 21:56:03 +0300

    Add tmux prepare script

---
+++ tmux.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+session="zeebe"
+
+attach() {
+    [ -n "${TMUX:-}" ] &&
+        tmux switch-client -t $session ||
+        tmux attach-session -t $session
+}
+
+if tmux has-session -t "=$session" 2> /dev/null; then
+    attach
+    exit 0
+fi
+
+
+tmux new-session -d -s $session
+
+# top panes
+tmux split-window -v
+tmux resize-pane -U 10
+tmux select-pane -U
+tmux split-window -h
+tmux resize-pane -R 20
+tmux send-keys -t $session 'watch -n1 "kubectl get pods -l app.kubernetes.io/instance=camunda"' Enter
+tmux select-pane -L
+tmux send-keys -t $session 'docker stats' Enter
+
+# right pane
+tmux select-pane -D
+tmux split-window -h
+tmux resize-pane -R 40
+tmux send-keys -t $session 'watch -n1 "zbctl --insecure status"' Enter
+tmux select-pane -L
+
+attach
